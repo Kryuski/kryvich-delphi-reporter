85b76ae821724cae50fa59a60dc912a28cebbb1c
Kryvich <44714498+Kryuski@users.noreply.github.com>
2025-03-03 01:00:43 +0300

    Delete clear.cmd

--- clear.cmd
+++
@@ -1,6 +0,0 @@
-del /f /s *.~* *.bak *.dcu *.dsk *.identcache *.local *.map *.drc *.exe *.stat *.otares
-rd /s /q __history
-rd /s /q __recovery
-rd /s /q Demo\__history
-rd /s /q Demo\__recovery
-del /q Demo\Output\*.*
